2e7daa9b873400c1631742544de0f8c8e84207f0
Adetayo Joye <ruyo99@ymail.com>
2025-12-03 06:11:07 +0000

    Add yml file to intermediate models

--- models/intermediate/int_nba_player_stats__all_players_season_97_22_and_rookies_and_playoffs.sql
+++ models/intermediate/int_nba_player_stats__all_players_season_97_22_and_rookies_and_playoffs.sql
@@ -1,19 +1,20 @@
 with players_stats_97_22_rookie_year as (
     select * 
-     from {{ ref('int_nba_player_stats__player_stats_97_22_and_rookie_players_names') }}
+    from {{ ref('int_nba_player_stats__player_stats_97_22_and_rookies_players_names') }}
 )
 ,playoffs_players as (
     select *
-     from {{ ref('int_nba_player_stats__players_playoffs') }}
+    from {{ ref('int_nba_player_stats__players_playoffs') }}
 )
 , rookie_playoffs_players as (
     select *
-     from players_stats_97_22_rookie_year
-     union
+    from players_stats_97_22_rookie_year
+    union
     select *
-      from playoffs_players
+    from playoffs_players
      
 )
 
-select * from rookie_playoffs_players
-  order by year_of_birth
+select * 
+from rookie_playoffs_players
+order by year_of_birth
--- models/intermediate/int_nba_player_stats__playoffs_players_added_team_name.sql
+++ models/intermediate/int_nba_player_stats__playoffs_players_added_team_name.sql
@@ -1,18 +1,17 @@
 with player_playoffs_stats as (
     select *
-      from {{ ref('stg_nba_player_stats__players_playoff') }}
+    from {{ ref('stg_nba_player_stats__players_playoff') }}
 )
 , nba_teams as (
     select * 
-      from {{ ref('stg_nba_player_stats__nba_teams') }}
+    from {{ ref('stg_nba_player_stats__nba_teams') }}
 )
 , add_teams_name as (
     select nt.team_name
           ,pp.*
-      from player_playoffs_stats pp 
-      left join nba_teams nt 
-      on nt.teams = pp.teams
-     order by pp.seasons
+    from player_playoffs_stats pp 
+    left join nba_teams nt on nt.teams = pp.teams
+    order by pp.seasons
 )
 
 select * from add_teams_name
